5c8f33c700086c4e34122b2e36021e8cfca2c0d9
Enzo Ma <enzoma852@gmail.com>
2026-03-25 19:25:58 +0200

    Updated function for pagination enveloppe

---
+++ dss2-backend/TodoApi/DTOs/Todos/CreateTodoRequest.cs
@@ -0,0 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+using TodoApi.Enums;
+
+namespace TodoApi.DTOs.Todos;
+
+public class CreateTodoRequest
+{
+    [Required]
+    [MinLength(3)]
+    [MaxLength(100)]
+    public string Title { get; set; } = string.Empty;
+
+    [MaxLength(1000)]
+    public string? Details { get; set; }
+
+    [Required]
+    public Priority Priority { get; set; } = Priority.medium;
+
+    public DateOnly? DueDate { get; set; }
+
+    public bool IsPublic { get; set; } = false;
+}
---
+++ dss2-backend/TodoApi/DTOs/Todos/PagedRespond.cs
@@ -0,0 +1,10 @@
+namespace TodoApi.DTOs.Todos;
+
+public class PagedResponse<T>
+{
+    public IEnumerable<T> Items { get; set; } = new List<T>();
+    public int Page { get; set; }
+    public int PageSize { get; set; }
+    public int TotalItems { get; set; }
+    public int TotalPages { get; set; }
+}
