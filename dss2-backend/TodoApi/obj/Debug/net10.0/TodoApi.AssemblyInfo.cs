b00f8ac8d44b7c523fc50f6bd8f1b0507e533aa1
Enzo Ma <enzoma852@gmail.com>
2026-03-30 02:35:13 +0300

    Update

--- dss2-backend/TodoApi/obj/Debug/net10.0/TodoApi.AssemblyInfo.cs
+++ dss2-backend/TodoApi/obj/Debug/net10.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c8f33c700086c4e34122b2e36021e8cfca2c0d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
